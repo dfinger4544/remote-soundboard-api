02ca9adfd97f45c4ec87751febe2c2510530a9e0
Daniel Finger <dfinger4544@gmail.com>
2024-05-21 18:13:23 -0500

    Device Shutdown Test

--- controllers/deviceController.ts
+++ controllers/deviceController.ts
@@ -1,5 +1,6 @@
-import * as os from "os";
 import { Request, Response, NextFunction } from "express";
+import * as os from "os";
+import { exec } from "child_process";
 
 const platform = os.platform();
 
@@ -14,18 +15,52 @@ export async function getPlatform(
     next(err);
   }
 }
+//remote-soundboard.local:8080
 export async function volumeUp(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+) {
+  try {
+    if (platform !== "linux") {
+      const error: any = new Error(`Setting is not accessible on ${platform}`);
+      error.statusCode = 503;
+      throw error;
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+
 export async function volumeDown(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+) {
+  try {
+    if (platform !== "linux") {
+      const error: any = new Error(`Setting is not accessible on ${platform}`);
+      error.statusCode = 503;
+      throw error;
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+
 export async function shutdown(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+) {
+  try {
+    if (platform !== "linux") {
+      const error: any = new Error(`Setting is not accessible on ${platform}`);
+      error.statusCode = 503;
+      throw error;
+    }
+    exec("sudo shutdown -h now");
+  } catch (err) {
+    next(err);
+  }
+}
--- routes/deviceRoutes.ts
+++ routes/deviceRoutes.ts
@@ -4,5 +4,8 @@ const router = express.Router();
 import * as deviceCtrl from "../controllers/deviceController";
 
 router.get("/platform", deviceCtrl.getPlatform);
+router.get("/volumeUp", deviceCtrl.volumeUp);
+router.get("/volumeDown", deviceCtrl.volumeDown);
+router.get("/shutdown", deviceCtrl.shutdown);
 
 export default router;
